9c6cbfe35c86016f7a81e7cf9c0dddefae2198f7
gajendra <gajendra.s@devstree.in>
2025-07-29 13:02:25 +0530

    tab issue of approval has fixed

--- src/routes/_authenticated/approvals/expense-category.tsx
+++ src/routes/_authenticated/approvals/expense-category.tsx
@@ -1,8 +1,8 @@
-import ExpenseCategory from "@/features/approvals/components/category/expenses-category";
+import ExpenseApprovalsPage from "@/features/approvals";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute(
   "/_authenticated/approvals/expense-category"
 )({
-  component: ExpenseCategory,
+  component: ExpenseApprovalsPage,
 });
