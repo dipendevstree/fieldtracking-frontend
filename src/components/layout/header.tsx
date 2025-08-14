c6838d04e93492e0f86df047aeca7a3bcc38b66d
gajendra <gajendra.s@devstree.in>
2025-08-14 12:49:15 +0530

    module name features  in header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { cn } from '@/lib/utils'
-import { Separator } from '@/components/ui/separator'
-import { SidebarTrigger } from '@/components/ui/sidebar'
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useModuleName } from "@/hooks/use-module-name";
 
 interface HeaderProps extends React.HTMLAttributes<HTMLElement> {
-  fixed?: boolean
-  ref?: React.Ref<HTMLElement>
+  fixed?: boolean;
+  ref?: React.Ref<HTMLElement>;
 }
 
 export const Header = ({
@@ -14,35 +15,37 @@ export const Header = ({
   children,
   ...props
 }: HeaderProps) => {
-  const [offset, setOffset] = React.useState(0)
+  const [offset, setOffset] = React.useState(0);
+  const pathName = useModuleName();
 
   React.useEffect(() => {
     const onScroll = () => {
-      setOffset(document.body.scrollTop || document.documentElement.scrollTop)
-    }
+      setOffset(document.body.scrollTop || document.documentElement.scrollTop);
+    };
 
     // Add scroll listener to the body
-    document.addEventListener('scroll', onScroll, { passive: true })
+    document.addEventListener("scroll", onScroll, { passive: true });
 
     // Clean up the event listener on unmount
-    return () => document.removeEventListener('scroll', onScroll)
-  }, [])
+    return () => document.removeEventListener("scroll", onScroll);
+  }, []);
 
   return (
     <header
       className={cn(
-        'bg-background flex h-16 items-center gap-3 p-4 sm:gap-4',
-        fixed && 'header-fixed peer/header fixed z-50 w-[inherit] rounded-md',
-        offset > 10 && fixed ? 'shadow-sm' : 'shadow-none',
+        "bg-background flex h-16 items-center gap-3 p-4 sm:gap-4",
+        fixed && "header-fixed peer/header fixed z-50 w-[inherit] rounded-md",
+        offset > 10 && fixed ? "shadow-sm" : "shadow-none",
         className
       )}
       {...props}
     >
-      <SidebarTrigger variant='outline' className='scale-125 sm:scale-100' />
-      <Separator orientation='vertical' className='h-6' />
+      <SidebarTrigger variant="outline" className="scale-125 sm:scale-100" />
+      <Separator orientation="vertical" className="h-6" />
+      <p className="font-medium">{pathName}</p>
       {children}
     </header>
-  )
-}
+  );
+};
 
-Header.displayName = 'Header'
+Header.displayName = "Header";
