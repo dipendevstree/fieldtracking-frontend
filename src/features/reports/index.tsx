128daf4d10ff1cd8e30535d72d2aac4d65336d56
gajendra-devstree <gajendra.s@devstree.in>
2025-10-11 09:44:23 +0530

    removed unused tab and ui and corrected according to  figma

--- src/features/reports/index.tsx
+++ src/features/reports/index.tsx
@@ -4,17 +4,9 @@ import { cn } from "@/lib/utils";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Main } from "@/components/layout/main";
 import AllReports from "./all-reports";
-import CustomerReports from "./components/customer-reports";
-import FieldActivityReports from "./components/field-activity";
-import ReportHistory from "./components/report-history";
-import PerformanceDashboard from "./components/performance";
+import CustomReports from "./components/custom-reports";
 
-export type ApprovalsTabValue =
-  | "/reports"
-  | "/reports/performance"
-  | "/reports/field-activity"
-  | "/reports/customer-reports"
-  | "/reports/report-history";
+export type ApprovalsTabValue = "/reports" | "/reports/custom-reports";
 
 export default function Reports() {
   const { latestLocation } = useRouter();
@@ -32,34 +24,19 @@ export default function Reports() {
         onValueChange={handleTabChange}
         className="space-y-5"
       >
-        <TabsList className="grid w-full grid-cols-5 mb-2 h-10">
+        <TabsList className="grid w-full grid-cols-2 mb-2 h-10">
           <TabsTrigger value="/reports">Expense Reports</TabsTrigger>
-          <TabsTrigger value="/reports/performance">Performance</TabsTrigger>
-          <TabsTrigger value="/reports/field-activity">
-            Field Activity
-          </TabsTrigger>
-          <TabsTrigger value="/reports/customer-reports">
-            Customer Reports
-          </TabsTrigger>
-          <TabsTrigger value="/reports/report-history">
-            Report History
+          <TabsTrigger value="/reports/custom-reports">
+            Custom Reports
           </TabsTrigger>
         </TabsList>
 
         <TabsContent value="/reports" className="space-y-4">
           <AllReports />
         </TabsContent>
-        <TabsContent value="/reports/performance" className="space-y-4">
-          <PerformanceDashboard />
-        </TabsContent>
-        <TabsContent value="/reports/field-activity" className="space-y-4">
-          <FieldActivityReports />
-        </TabsContent>
-        <TabsContent value="/reports/customer-reports" className="space-y-4">
-          <CustomerReports />
-        </TabsContent>
-        <TabsContent value="/reports/report-history" className="space-y-4">
-          <ReportHistory />
+
+        <TabsContent value="/reports/custom-reports" className="space-y-4">
+          <CustomReports />
         </TabsContent>
       </Tabs>
     </Main>
