81b77f327f8bb20c368819d1cfc46e5a215879c6
gajendra-devstree <gajendra.s@devstree.in>
2025-10-28 09:26:23 +0530

    added condition to render correct side bar menus

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { useNavigate } from '@tanstack/react-router'
+import React from "react";
+import { useNavigate } from "@tanstack/react-router";
 import {
   IconArrowRightDashed,
   IconChevronRight,
   // IconDeviceLaptop,
   // IconMoon,
   // IconSun,
-} from '@tabler/icons-react'
-import { useSearch } from '@/context/search-context'
+} from "@tabler/icons-react";
+import { useSearch } from "@/context/search-context";
 // import { useTheme } from '@/context/theme-context'
 import {
   CommandDialog,
@@ -17,30 +17,39 @@ import {
   CommandItem,
   CommandList,
   // CommandSeparator,
-} from '@/components/ui/command'
-import { sidebarData } from './layout/data/sidebar-data'
-import { ScrollArea } from './ui/scroll-area'
+} from "@/components/ui/command";
+import {
+  sidebarData,
+  SUPER_ADMIN_SIDEBAR_DATA,
+} from "./layout/data/sidebar-data";
+import { ScrollArea } from "./ui/scroll-area";
+import { useAuthStore } from "@/stores/use-auth-store";
 
 export function CommandMenu() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   // const { setTheme } = useTheme()
-  const { open, setOpen } = useSearch()
+  const { open, setOpen } = useSearch();
+  const { user } = useAuthStore();
 
   const runCommand = React.useCallback(
     (command: () => unknown) => {
-      setOpen(false)
-      command()
+      setOpen(false);
+      command();
     },
     [setOpen]
-  )
+  );
+
+  const isSuperAdmin = user?.isSuperAdmin;
+
+  const SIDEBAR_DATA = isSuperAdmin ? SUPER_ADMIN_SIDEBAR_DATA : sidebarData;
 
   return (
     <CommandDialog modal open={open} onOpenChange={setOpen}>
-      <CommandInput placeholder='Type a command or search...' />
+      <CommandInput placeholder="Type a command or search..." />
       <CommandList>
-        <ScrollArea type='hover' className='h-72 pr-1'>
+        <ScrollArea type="hover" className="h-72 pr-1">
           <CommandEmpty>No results found.</CommandEmpty>
-          {sidebarData.navGroups.map((group) => (
+          {SIDEBAR_DATA?.navGroups?.map((group) => (
             <CommandGroup key={group.title} heading={group.title}>
               {group.items.map((navItem, i) => {
                 if (navItem.url)
@@ -49,30 +58,30 @@ export function CommandMenu() {
                       key={`${navItem.url}-${i}`}
                       value={navItem.title}
                       onSelect={() => {
-                        runCommand(() => navigate({ to: navItem.url }))
+                        runCommand(() => navigate({ to: navItem.url }));
                       }}
                     >
-                      <div className='mr-2 flex h-4 w-4 items-center justify-center'>
-                        <IconArrowRightDashed className='text-muted-foreground/80 size-2' />
+                      <div className="mr-2 flex h-4 w-4 items-center justify-center">
+                        <IconArrowRightDashed className="text-muted-foreground/80 size-2" />
                       </div>
                       {navItem.title}
                     </CommandItem>
-                  )
+                  );
 
                 return navItem.items?.map((subItem, i) => (
                   <CommandItem
                     key={`${navItem.title}-${subItem.url}-${i}`}
                     value={`${navItem.title}-${subItem.url}`}
                     onSelect={() => {
-                      runCommand(() => navigate({ to: subItem.url }))
+                      runCommand(() => navigate({ to: subItem.url }));
                     }}
                   >
-                    <div className='mr-2 flex h-4 w-4 items-center justify-center'>
-                      <IconArrowRightDashed className='text-muted-foreground/80 size-2' />
+                    <div className="mr-2 flex h-4 w-4 items-center justify-center">
+                      <IconArrowRightDashed className="text-muted-foreground/80 size-2" />
                     </div>
                     {navItem.title} <IconChevronRight /> {subItem.title}
                   </CommandItem>
-                ))
+                ));
               })}
             </CommandGroup>
           ))}
@@ -93,5 +102,5 @@ export function CommandMenu() {
         </ScrollArea>
       </CommandList>
     </CommandDialog>
-  )
+  );
 }
