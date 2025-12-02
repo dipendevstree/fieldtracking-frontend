a6b2df6b8cfa83a25de1d860213c82307b678944
gajendra-devstree <gajendra.s@devstree.in>
2025-12-02 14:17:29 +0530

    fixed global filter issue

--- src/features/UserManagement/index.tsx
+++ src/features/UserManagement/index.tsx
@@ -29,13 +29,15 @@ const AllUsers = () => {
     user?.organization?.allowAddUsersBasedOnTerritories;
 
   const { filters, setFilters, setOpen, open } = useUsersStore();
-  const { noAdmin } = useSearch({ from: "/_authenticated/user-management/" } as any);
+  const { noAdmin } = useSearch({
+    from: "/_authenticated/user-management/",
+  } as any);
   const queryParams = useMemo((): any => {
     const params: any = {
       ...pagination,
       searchFor: filters.search || "",
       roleId: filters.roleId || "",
-      noAdmin
+      noAdmin,
     };
 
     if (allowTerritoryFilter) {
@@ -119,7 +121,7 @@ const AllUsers = () => {
     {
       key: "roleId",
       type: "searchable-select",
-      placeholder: "Role",
+      placeholder: "Select Role",
       value: filters.roleId,
       onChange: (value) => setFilters({ roleId: value ?? "" }),
       onCancelPress: () => setFilters({ roleId: "" }),
