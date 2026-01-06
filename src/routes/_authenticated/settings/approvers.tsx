d0083a6e01f7977e310b652937f49cd4faba3ede
mukeshdevs <mukesh.s@devstree.in>
2026-01-06 12:59:12 +0530

    name change to proper routes path

--- src/routes/_authenticated/settings/approvers.tsx
+++ src/routes/_authenticated/settings/approvers.tsx
@@ -2,11 +2,9 @@ import ApproversPage from "@/features/settings/Approvers";
 import { ProtectedRoute } from "@/permissions/components/ProtectedRoute";
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/_authenticated/settings/temp-approvers")(
-  {
-    component: RouteComponent,
-  }
-);
+export const Route = createFileRoute("/_authenticated/settings/approvers")({
+  component: RouteComponent,
+});
 
 function RouteComponent() {
   return (
