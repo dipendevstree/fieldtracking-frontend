298e5bb2bf7ac45fb218b17783926743e7ec4c62
mukeshdevs <mukesh.s@devstree.in>
2026-01-06 12:57:59 +0530

    name change to temp

--- src/routes/_authenticated/settings/temp-approvers.tsx
+++ src/routes/_authenticated/settings/temp-approvers.tsx
@@ -2,9 +2,11 @@ import ApproversPage from "@/features/settings/Approvers";
 import { ProtectedRoute } from "@/permissions/components/ProtectedRoute";
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/_authenticated/settings/approvers")({
-  component: RouteComponent,
-});
+export const Route = createFileRoute("/_authenticated/settings/temp-approvers")(
+  {
+    component: RouteComponent,
+  }
+);
 
 function RouteComponent() {
   return (
