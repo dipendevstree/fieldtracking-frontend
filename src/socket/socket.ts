762559ce943d32c1aa734d02322803b695f6aeb0
mukeshdevs <mukesh.s@devstree.in>
2026-01-23 23:19:58 +0530

    socket uri changes

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,8 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://userfieldtrack360-api.devstree.in/");
+export const socket = io(
+  import.meta.env.VITE_LIVE_TRACKING_API_URL!.replace("/api/v1", ""),
+);
 
 export const socketForVisit = (token: any) =>
-  io("https://fieldtrack360-api.devstree.in/", {
+  io(import.meta.env.VITE_API_URL!.replace("/api/v1", ""), {
     extraHeaders: { Authorization: `Bearer ${token}` },
   });
