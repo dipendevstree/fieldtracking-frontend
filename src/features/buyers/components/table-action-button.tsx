1b7b12c9340138554833f2d357fefae00e18e31d
gajendra-devstree <gajendra.s@devstree.in>
2026-02-04 18:23:04 +0530

    fixed react node issue

--- src/features/buyers/components/table-action-button.tsx
+++ src/features/buyers/components/table-action-button.tsx
@@ -1,29 +1,29 @@
-import { DotsHorizontalIcon } from '@radix-ui/react-icons'
-import { Row } from '@tanstack/react-table'
-import { IconEdit } from '@tabler/icons-react'
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { Row } from "@tanstack/react-table";
+import { IconEdit } from "@tabler/icons-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuShortcut,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import CustomButton from '@/components/shared/custom-button'
-import CustomTooltip from '@/components/shared/custom-tooltip'
-import { useUsersStore } from '../store/user.store'
-import { User } from '../types'
+} from "@/components/ui/dropdown-menu";
+import CustomButton from "@/components/shared/custom-button";
+import CustomTooltip from "@/components/shared/custom-tooltip";
+import { useUsersStore } from "../store/user.store";
+import { User } from "../types";
 
 interface DataTableRowActionsProps {
-  row: Row<User>
+  row: Row<User>;
 }
 
 export function DataTableRowActions({ row }: DataTableRowActionsProps) {
-  const { setOpen, setCurrentRow } = useUsersStore()
+  const { setOpen, setCurrentRow } = useUsersStore();
 
   const handleEdit = (row: Row<User>) => {
-    setOpen('edit')
-    setCurrentRow(row.original)
-  }
+    setOpen("edit");
+    setCurrentRow(row.original);
+  };
 
   // const handleDelete = (row: Row<User>) => {
   //     setOpen('delete')
@@ -34,16 +34,18 @@ export function DataTableRowActions({ row }: DataTableRowActionsProps) {
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
         <CustomButton
-          variant='ghost'
-          className='data-[state=open]:bg-muted flex h-8 w-8 cursor-pointer p-0'
+          variant="ghost"
+          className="data-[state=open]:bg-muted flex h-8 w-8 cursor-pointer p-0"
         >
-          <CustomTooltip title='Actions'>
-            <DotsHorizontalIcon className='h-4 w-4' />
-            <span className='sr-only'>Open menu</span>
+          <CustomTooltip title="Actions">
+            <div>
+              <DotsHorizontalIcon className="h-4 w-4" />
+              <span className="sr-only">Open menu</span>
+            </div>
           </CustomTooltip>
         </CustomButton>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end' className='w-[160px]'>
+      <DropdownMenuContent align="end" className="w-[160px]">
         <DropdownMenuItem onClick={() => handleEdit(row)}>
           Edit
           <DropdownMenuShortcut>
@@ -62,5 +64,5 @@ export function DataTableRowActions({ row }: DataTableRowActionsProps) {
                 </DropdownMenuItem> */}
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
