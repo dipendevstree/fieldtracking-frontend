b242c988d9e391b1a7cb9cc8e988f485a24ba5a0
gajendra-devstree <gajendra.s@devstree.in>
2026-02-05 15:21:59 +0530

    added dynamic page size features

--- src/features/driver/components/table.tsx
+++ src/features/driver/components/table.tsx
@@ -1,26 +1,41 @@
-import { CustomDataTable } from "@/components/shared/custom-data-table"
-import { ColumnDef } from "@tanstack/react-table"
+import { CustomDataTable } from "@/components/shared/custom-data-table";
+import { ColumnDef } from "@tanstack/react-table";
 
-import { columns } from "./columns"
-import { Driver } from "../types"
-import { PaginationCallbacks } from "@/components/shared/custom-table-pagination"
-
+import { columns } from "./columns";
+import { Driver } from "../types";
+import { PaginationCallbacks } from "@/components/shared/custom-table-pagination";
 
 interface UserTableProps {
-    data: Driver[]
-    totalCount: number
-    loading?: boolean
-    paginationCallbacks?: PaginationCallbacks
-    currentPage?: number
+  data: Driver[];
+  totalCount: number;
+  loading?: boolean;
+  paginationCallbacks?: PaginationCallbacks;
+  currentPage?: number;
+  pageSize?: number;
 }
 
-const MerchantTable = ({ data, totalCount, loading, paginationCallbacks, currentPage }: UserTableProps) => {
-    return (
-        <div className='-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-y-0 lg:space-x-12'>
-            <CustomDataTable currentPage={currentPage} loading={loading} data={data} columns={columns as ColumnDef<unknown>[]} totalCount={totalCount} key={'merchants'}
-                paginationCallbacks={paginationCallbacks} />
-        </div>
-    )
-}
+const MerchantTable = ({
+  data,
+  totalCount,
+  loading,
+  paginationCallbacks,
+  currentPage,
+  pageSize,
+}: UserTableProps) => {
+  return (
+    <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-y-0 lg:space-x-12">
+      <CustomDataTable
+        currentPage={currentPage}
+        loading={loading}
+        data={data}
+        columns={columns as ColumnDef<unknown>[]}
+        totalCount={totalCount}
+        defaultPageSize={pageSize}
+        key={"merchants"}
+        paginationCallbacks={paginationCallbacks}
+      />
+    </div>
+  );
+};
 
-export default MerchantTable
+export default MerchantTable;
--- src/features/merchants/index.tsx
+++ src/features/merchants/index.tsx
@@ -1,48 +1,48 @@
-import { useState } from 'react'
+import { useState } from "react";
 import {
   ALL_PAGE_META_DATA,
   DEFAULT_PAGE_NUMBER,
   DEFAULT_PAGE_SIZE,
-} from '@/data/app.data'
-import TablePageLayout from '@/components/layout/table-page-layout'
-import { ErrorPage } from '@/components/shared/custom-error'
-import { MerchantsActionModal } from './components/action-form-modal'
-import MerchantTable from './components/table'
-import { useGetMerchants } from './services/merchants.hook'
-import { useUsersStore } from './store/merchant.store'
-import { ErrorResponse } from './types'
+} from "@/data/app.data";
+import TablePageLayout from "@/components/layout/table-page-layout";
+import { ErrorPage } from "@/components/shared/custom-error";
+import { MerchantsActionModal } from "./components/action-form-modal";
+import MerchantTable from "./components/table";
+import { useGetMerchants } from "./services/merchants.hook";
+import { useUsersStore } from "./store/merchant.store";
+import { ErrorResponse } from "./types";
 
 const SellersPage = () => {
   const [pagination, setPagination] = useState({
     page: DEFAULT_PAGE_NUMBER,
     limit: DEFAULT_PAGE_SIZE,
-  })
-  const { setOpen } = useUsersStore()
-  const { description, title } = ALL_PAGE_META_DATA.sellers
+  });
+  const { setOpen } = useUsersStore();
+  const { description, title } = ALL_PAGE_META_DATA.sellers;
   const {
     totalCount = 0,
     merchants = [],
     isLoading,
     error,
-  } = useGetMerchants(pagination)
+  } = useGetMerchants(pagination);
   // const { setOpen } = useUsersStore()
 
   if (error) {
-    const errorResponse = (error as ErrorResponse)?.response?.data
+    const errorResponse = (error as ErrorResponse)?.response?.data;
     return (
       <ErrorPage
         errorCode={errorResponse?.statusCode}
         message={errorResponse?.message}
       />
-    )
+    );
   }
   const handleAddMerchant = () => {
-    setOpen('add')
-  }
+    setOpen("add");
+  };
 
   const onPaginationChange = (page: number, pageSize: number) => {
-    setPagination((prev) => ({ ...prev, page, limit: pageSize }))
-  }
+    setPagination((prev) => ({ ...prev, page, limit: pageSize }));
+  };
   return (
     <TablePageLayout
       title={title}
@@ -54,11 +54,13 @@ const SellersPage = () => {
         totalCount={totalCount}
         loading={isLoading}
         currentPage={pagination.page}
+        pageSize={pagination.limit}
         paginationCallbacks={{ onPaginationChange }}
       />
-      <MerchantsActionModal key={'merchants-action-modal'} />
-    </TablePageLayout>
-  )
-}
 
-export default SellersPage
+      <MerchantsActionModal key={"merchants-action-modal"} />
+    </TablePageLayout>
+  );
+};
+
+export default SellersPage;
