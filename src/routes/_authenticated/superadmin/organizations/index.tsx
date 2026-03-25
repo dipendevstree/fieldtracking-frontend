a4ab43e946a8791701c0dade8d84582467a37257
gajendra-devstree <gajendra.s@devstree.in>
2026-03-25 10:27:27 +0530

    added plan status filter and redirect features with filter from org

--- src/routes/_authenticated/superadmin/organizations/index.tsx
+++ src/routes/_authenticated/superadmin/organizations/index.tsx
@@ -1,14 +1,19 @@
-import { createFileRoute } from '@tanstack/react-router'
-import Organizations from '@/features/organizations'
+import { createFileRoute } from "@tanstack/react-router";
+import Organizations from "@/features/organizations";
 
-export const Route = createFileRoute('/_authenticated/superadmin/organizations/')({
+export const Route = createFileRoute(
+  "/_authenticated/superadmin/organizations/",
+)({
+  validateSearch: (search: Record<string, unknown>) => ({
+    planStatus: (search.planStatus as string) ?? "",
+  }),
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
     // <ProtectedRoute requiredPermission='organizations'>
     // </ProtectedRoute>
     <Organizations />
-  )
+  );
 }
